fe5c2efcd410e76b1fd3b65723f9efaaff7b1c1f
ngHaiDangDev <nguyenhaidang04.work@gmail.com>
2024-04-20 14:33:33 +0700

    UI Audio Manager debug

--- Assets/Scripts/UILevel/UIControllerLevel.cs
+++ Assets/Scripts/UILevel/UIControllerLevel.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +6,23 @@ using UnityEngine.UI;
 public class UIControllerLevel : MonoBehaviour
 {
     public Slider _musicSlider, _sfxSlider;
+    public Image fillmusic, fillsfx;
+
+    private void Start()
+    {
+        float fillMusic = PlayerPrefs.GetFloat("Mucsic Slider", 1f);
+        float fillSFX = PlayerPrefs.GetFloat("SFX Slider", 1f);
+
+        fillmusic.fillAmount = fillMusic;
+        fillsfx.fillAmount = fillSFX;
+
+        _musicSlider.value = fillMusic;
+        _sfxSlider.value = fillSFX;
+
+        _musicSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        _sfxSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
 
     public void MusicVolume()
     {
@@ -15,5 +32,19 @@ public class UIControllerLevel : MonoBehaviour
     public void SFXVolume()
     {
         AudioManager.instance.SFXVolume(_sfxSlider.value);
+    }
+
+    private void ValueChangeCheck()
+    {
+        float musicSlider = _musicSlider.value;
+        float sfxSlider = _sfxSlider.value;
+
+        // Thay đổi fill area
+        fillmusic.fillAmount = musicSlider;
+        fillsfx.fillAmount = sfxSlider;
+
+        PlayerPrefs.SetFloat("Mucsic Slider", _musicSlider.value);
+        PlayerPrefs.SetFloat("SFX Slider", _sfxSlider.value);
+        PlayerPrefs.Save();
     }
 }
